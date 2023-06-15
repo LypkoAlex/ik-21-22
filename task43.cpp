a3191c6e9e8917363909cc8ddba130e7e8fabf2e
Kirill-cyber-cat <8096877@gmail.com>
2023-06-15 23:56:41 +0300

    Коментар

--- task43.cpp
+++ task43.cpp
@@ -6,15 +6,15 @@ int main() {
     setlocale(LC_ALL, "");
     double length, width, area;
 
-    // Введення довжини прямокутника
+    
     cout << "Введіть довжину прямокутника: ";
     cin >> length;
 
-    // Введення ширини прямокутника
+    
     cout << "Введіть ширину прямокутника: ";
     cin >> width;
 
-    // Обчислення площі прямокутника
+    
     area = length * width;
 
     cout << "Площа прямокутника: " << area << endl;
